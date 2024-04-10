a1c0eb57a5f74141c461314fa9af2eef51bb92df
Ruth Sánchez P <rh.sanchez@hotmail.com>
2024-04-10 18:15:56 -0400

    Create encuesta_PF.js

---
+++ encuesta_PF.js
@@ -0,0 +1,15 @@
+const esPositivo = numero => numero > 0;
+
+const validarCantidadRespuestas = (cantidad) => {
+  if (!isNaN(cantidad) && esPositivo(cantidad)) {
+    return cantidad;
+  } else {
+    console.log("Ingresar valor numerico.");
+  }
+};
+
+function crearEncuesta() {
+  let cantidadPreguntas = parseInt(prompt("¿Cuántas preguntas tendrá la encuesta?"));
+  if (!esPositivo(cantidadPreguntas)) {
+    console.log("Debe ingresar un número válido de preguntas.");
+  }
