b082a9d0f68393b4f11490d59bf995d835bfeca5
ruths <rh.sanchez@hotmail.com>
2024-04-13 17:17:34 -0400

    Update encuesta_PF.js

--- pf/encuesta_PF.js
+++ pf/encuesta_PF.js
@@ -1,15 +1,23 @@
 const esPositivo = numero => numero > 0;
 
-const validarCantidadRespuestas = (cantidad) => {
+const validarCantidadPreguntas = (cantidad) => {
+  cantidad = parseInt(cantidad);
   if (!isNaN(cantidad) && esPositivo(cantidad)) {
+    //console.log(`Usted va a ingresar ${cantidad} preguntas`);
     return cantidad;
-  } else {
-    console.log("Ingresar valor numerico.");
   }
-};
+  else {
+    console.log("Ingresar valor numérico.");
+    return undefined;
+  }
+}
 
 function crearEncuesta() {
-  let cantidadPreguntas = parseInt(prompt("¿Cuántas preguntas tendrá la encuesta?"));
-  if (!esPositivo(cantidadPreguntas)) {
-    console.log("Debe ingresar un número válido de preguntas.");
-  }
+  const cantidad = prompt("Ingrese cantidad de preguntas:");
+  const cantidadPreguntas = validarCantidadPreguntas(cantidad);
+  console.log(`Se crearán ${cantidadPreguntas} preguntas `)
+
+}
+
+//Cantidad de preguntas que tendrá la encuesta
+crearEncuesta()
