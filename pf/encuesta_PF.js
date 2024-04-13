57fb1503956e1ca5bbef62256a88f2d3c7d7b2a5
ruths <rh.sanchez@hotmail.com>
2024-04-13 17:53:17 -0400

    Update encuesta_PF.js

--- pf/encuesta_PF.js
+++ pf/encuesta_PF.js
@@ -1,36 +1,43 @@
 const esPositivo = numero => numero > 0;
 
 const validarCantidad = (cantidad) => {
-  cantidad = parseInt(cantidad);
+  cantidad = parseInt(cantidad, 10);
   if (!isNaN(cantidad) && esPositivo(cantidad)) {
-    //console.log(`Usted va a ingresar ${cantidad} preguntas`);
     return cantidad;
-  }
-  else {
+  } else {
     console.log("Ingresar valor numérico.");
     return undefined;
   }
 }
 
 function crearEncuesta() {
-
-  encuesta = [];
+  let encuesta = [];
 
   const pregunta = prompt("Ingrese una pregunta:");
   console.log(`Su pregunta ingresada es ${pregunta}`);
   const cantidad = prompt("Ingrese cantidad de opciones:");
   const cantidadOpciones = validarCantidad(cantidad);
-  console.log(`La pregunta tendrá: ${cantidadOpciones} opciones`);
 
-  let opciones = [];
+  if (cantidadOpciones) {
+    console.log(`La pregunta tendrá: ${cantidadOpciones} opciones`);
 
-  for (let i = 0; i < cantidadOpciones; i++) {
-    let opcion = prompt(`Opción ${i + 1}:`);
-    opciones.push(opcion);
+    let opciones = [];
+
+    for (let i = 0; i < cantidadOpciones; i++) {
+      let opcion = prompt(`Opción ${i + 1}:`);
+      opciones = [...opciones, opcion];  
+    }
+
+    encuesta = [...encuesta, { pregunta: pregunta, opciones: opciones }];
+    console.log(encuesta);
+  } else {
+    console.log("error.");
   }
-  encuesta.push({ pregunta: pregunta, opciones: opciones });
-  console.log(encuesta);
+  
   return encuesta;
 }
 
-crearEncuesta()
+function votarEncuesta() {
+}
+
+crearEncuesta();
